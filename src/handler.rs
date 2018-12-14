db095054883c4ceb9d5ff7f8810786f47fe825db
石博文 <sbw@sbw.so>
2018-12-14 22:30:38 +0800

    refactor: bump dependencies version
    fix compiler error

--- src/handler.rs
+++ src/handler.rs
@@ -1,70 +1,77 @@
 
+use std::io;
 use std::time::Duration;
 
-use trust_dns::op::message::Message;
-use trust_dns::op::query::Query;
+use trust_dns::op::Message;
+use trust_dns::op::Query;
 use trust_dns::rr::resource::Record;
+use trust_dns_proto::op::header::Header;
 use trust_dns_server::server::*;
+use trust_dns_server::authority::MessageResponseBuilder;
 use trust_dns_resolver::Resolver;
 use trust_dns_resolver::lookup::Lookup;
 use trust_dns_resolver::config::*;
 
 pub struct MyDNSHandler {
-    resolver: Resolver,
 }
 
 impl MyDNSHandler {
     pub fn new() -> MyDNSHandler {
 
+        Self {
+        }
+
         // 8.8.8.8:53 over tcp
-        let google_tcp = NameServerConfig {
-                            socket_addr: "8.8.8.8:53".parse().unwrap(),
-                            protocol: Protocol::Tcp,
-                         };
-        let local_dnscrypt = NameServerConfig {
-            socket_addr: "127.0.0.1:40".parse().unwrap(),
-            protocol: Protocol::Udp,
-        };
-        let mut res_cfg = ResolverConfig::new();
-        res_cfg.add_name_server(google_tcp);
-        // res_cfg.add_name_server(local_dnscrypt);
-        let mut res_opts = ResolverOpts::default();
-        res_opts.timeout = Duration::new(1, 0);
-        let res = Resolver::new(res_cfg, res_opts).unwrap();
+        //let google_tcp = NameServerConfig {
+                            //socket_addr: "8.8.8.8:53".parse().unwrap(),
+                            //protocol: Protocol::Tcp,
+                         //};
+        //let local_dnscrypt = NameServerConfig {
+            //socket_addr: "127.0.0.1:40".parse().unwrap(),
+            //protocol: Protocol::Udp,
+        //};
+        //let mut res_cfg = ResolverConfig::new();
+        //res_cfg.add_name_server(google_tcp);
+        //// res_cfg.add_name_server(local_dnscrypt);
+        //let mut res_opts = ResolverOpts::default();
+        //res_opts.timeout = Duration::new(1, 0);
+        //let res = Resolver::new(res_cfg, res_opts).unwrap();
 
-        MyDNSHandler {
-            resolver: res,
-        }
+        //MyDNSHandler {
+            //resolver: res,
+        //}
     }
 
-    fn lookup(&self, query: &Query) -> Option<Lookup> {
-        let addr = query.name().to_string();
-        let record_type = query.query_type();
+    //fn lookup(&self, query: &Query) -> Option<Lookup> {
+        //let addr = query.name().to_string();
+        //let record_type = query.query_type();
 
-        if let Ok(r) = self.resolver.lookup(&addr, record_type) {
-            return Some(r);
-        }
+        //if let Ok(r) = self.resolver.lookup(&addr, record_type) {
+            //return Some(r);
+        //}
 
-        None
-    }
+        //None
+    //}
 }
 
 impl RequestHandler for MyDNSHandler {
-    fn handle_request(&self, request: &Request) -> Message {
+    fn handle_request<'q, 'a, R: ResponseHandler + 'static>(
+        &'a self,
+        request: &'q Request,
+        response_handle: R
+    ) -> io::Result<()> {
 
-        let ref msg = request.message;
-        let mut response = msg.clone();
-
-        for q in msg.queries() {
-            let lookup = self.lookup(&q);
-            if lookup.is_none() { continue; }
-
-            let record_type = q.query_type();
-            response.add_answers(lookup.unwrap().iter().map(|rdata| {
-                Record::from_rdata(q.name().clone(), 100, record_type, rdata.clone())
-            }));
+        for query in request.message.queries() {
+            trace!("Query: {:?}", query.name());
         }
 
-        response
+        let message = &request.message;
+
+        let mut header = Header::new();
+        header.set_id(message.id());
+
+        // Just return a empty now
+        let response = MessageResponseBuilder::new(None).build(header);
+        response_handle.send_response(response)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
 
+#[macro_use]
+extern crate log;
+extern crate pretty_env_logger;
+extern crate tokio;
+extern crate futures;
 extern crate trust_dns;
 extern crate trust_dns_server;
 extern crate trust_dns_resolver;
+extern crate trust_dns_proto;
 
-use std::net::UdpSocket;
+use std::io;
+use std::net::SocketAddr;
 
+use futures::future;
+use futures::future::Future;
+use tokio::runtime::Runtime;
+use tokio::net::UdpSocket;
 use trust_dns_server::server::*;
 
 mod handler;
+
 use handler::MyDNSHandler;
 
 fn main() {
-    let handler = MyDNSHandler::new();
+    pretty_env_logger::init();
 
-    let mut server = ServerFuture::new(handler).unwrap();
-    // server.register_socket(UdpSocket::bind("127.0.0.1:4555").unwrap());
-    server.register_socket(UdpSocket::bind("127.0.0.1:53").unwrap());
-    server.listen().unwrap();
+    let addr: SocketAddr = "127.0.0.1:5353".parse().unwrap();
+    let socket = UdpSocket::bind(&addr).expect("bind fail");
+
+    let handler = MyDNSHandler::new();
+    let server = ServerFuture::new(handler);
+
+    let server_future: Box<Future<Item = (), Error = ()> + Send> =
+        Box::new(future::lazy(move || {
+            server.register_socket(socket);
+            future::empty()
+        }));
+
+    tokio::run(server_future.map_err(|e| { error!("{:#?}", e); }));
 }
